0f12ef1a0c263f0ba68803a5044501a98904b03b
Uğur Arıcı <m.ugurarici@gmail.com>
2025-01-26 16:14:57 +0300

    always redirect to dashboard

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\DirectoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 Route::get('/dashboard', function () {
